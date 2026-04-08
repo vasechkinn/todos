db7e7e41934f4b90742c8a952367aeb08d090f31
vasechkin <vasechkin@gmail.com>
2026-04-08 22:30:40 +0300

    feat: get by task id

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, Path, Query, HTTPException, status
 from typing import Annotated
 from dict_todo import TODOS, ID, Taska
 from dataclasses import dataclass, asdict
 
+def create_dict(list_tasks: list[Taska]):
+    return [asdict(task) for task in list_tasks]
 
 app = FastAPI()
 
@@ -20,4 +22,12 @@ async def get_todos(limit: Annotated[int | None, Query(ge=1)] = None, is_complet
     if limit is not None and limit <= len(tasks):
         tasks = tasks[:limit]
     
-    return {'tasks': [asdict(task) for task in tasks]}
+    return {'tasks': create_dict(tasks)}
+
+@app.get('/todos/{todo_id}')
+async def get_todo_by_id(todo_id: Annotated[int, Path(ge=1)]) -> dict:
+    for task in TODOS:
+        if task.id == todo_id:
+            return {'task': asdict(task)}
+    
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= 'task not found')
