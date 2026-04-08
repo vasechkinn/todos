0b9af2bbeb84456e3274bcca1e3cd3706798c2ae
vasechkin <vasechkin@gmail.com>
2026-04-08 22:51:24 +0300

    fix: search by title

--- main.py
+++ main.py
@@ -13,16 +13,24 @@ async def welcome() -> dict:
     return {'hello': 'hello'}
 
 @app.get('/todos')
-async def get_todos(limit: Annotated[int | None, Query(ge=1)] = None, is_completed: Annotated[bool | None, Query()] = None):
+async def get_todos(limit: Annotated[int | None, Query(ge=1)] = None, is_completed: Annotated[bool | None, Query()] = None, search: Annotated[str | None, Query(min_length=2)] = None):
     tasks = TODOS
-    
+
+    if search is not None:
+        tasks = [task for task in tasks if search.lower() in task.title.lower()]
+
     if is_completed is not None:
         tasks = [task for task in tasks if task.is_completed == is_completed]
     
     if limit is not None and limit <= len(tasks):
         tasks = tasks[:limit]
+
+    res = {'tasks': create_dict(tasks)}
     
-    return {'tasks': create_dict(tasks)}
+    if not tasks:
+        res['message'] = 'not found'
+    
+    return res
 
 @app.get('/todos/{todo_id}')
 async def get_todo_by_id(todo_id: Annotated[int, Path(ge=1)]) -> dict:
